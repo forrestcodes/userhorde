cf7657b428eab25a4595470e242db87a1135125d
forrest <forrestcodes@gmail.com>
2020-03-02 15:46:33 -0700

    Base models and seed data setup. Cleaned up gemfile and added bcrypt and faker gems.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Building seed data..."
+
+
+Account.transaction do
+
+  User.delete_all
+  Account.delete_all
+
+  account = Account.create!(phone: Faker::PhoneNumber.cell_phone, login: "Trainual", password: 'Trainual1')
+  users = []
+
+  1000.times do
+    users.push(
+        {
+            name: Faker::Name.name,
+            email: Faker::Internet.email,
+            title: Faker::Job.title,
+            phone: Faker::PhoneNumber.cell_phone,
+            active: [true, true, true, false].sample,
+            account_id: account.id,
+            updated_at: Faker::Date.between(from: 2.years.ago, to: Date.today),
+            created_at: Faker::Date.between(from: 2.years.ago, to: Date.today)
+        }
+    )
+    print '.'
+  end
+
+  puts ''
+  puts 'Inserting seed data into db...'
+
+  User.insert_all(users)
+
+  puts 'Done!'
+  puts "#{Account.count} Accounts created."
+  puts "#{User.count} Users created."
+end
